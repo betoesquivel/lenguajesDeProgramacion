73d552fd7f03da7d6c5ebe369e15274683118aec
betoesquivel <ppbeto94@gmail.com>
2014-08-29 00:01:49 -0500

    Removed extra letters, it now works just for the vowels as the problem requested.

--- scanner.py
+++ scanner.py
@@ -37,13 +37,8 @@ def filtro(c):
         return 6
     elif c == '$': # fin de entrada
         return 7
-    elif c == 'A' or c == 'B' or c == 'C' or c == 'D' or \
-         c == 'E' or c == 'F' or c == 'G' or c == 'H' or \
-         c == 'I' or c == 'J' or c == 'K' or c == 'L' or \
-         c == 'M' or c == 'N' or c == 'O' or c == 'P' or \
-         c == 'Q' or c == 'R' or c == 'S' or c == 'T' or \
-         c == 'U' or c == 'V' or c == 'W' or c == 'X' or \
-         c == 'Y' or c == 'Z':
+    elif c == 'A' or c == 'E' or c == 'I' or  \
+         c == 'O' or c == 'U':
         return 8
     elif c == '_':
         return 9
